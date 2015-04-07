f8618a9e3da83a7e5464b7d58fc67ab672d8e684
hirats <hirats@rpi.edu>
2015-04-07 03:15:18 -0400

    Update index.php

--- clubform/index.php
+++ clubform/index.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-    if (!isset($_SESSION['myusername'])) {
-        echo "You must be logged in to view this page";
-        exit(0);
-    }
-?>
+    //session_start();
+    //if (!isset($_SESSION['myusername'])) {
+        //echo "You must be logged in to view this page";
+        //exit(0);
+    //}
+?>  
 <?php 
     include ( "../header.php" ); 
 ?>
@@ -15,12 +15,41 @@
         <form method="post" action="submit.php">
             <div class="account" id="accountname"><//?php echo "$username"; ?></div>
             <div class="account" id="accountleft">
+                
+                <script>
+                    var counter = 0;
+
+                    function moreFields() {
+                        counter++;
+                        if(counter>3)
+                            return;
+                        var newFields = document.getElementById('readroot').cloneNode(true);
+                        newFields.id = '';
+                        newFields.style.display = 'block';
+                        var newField = newFields.childNodes;
+                        for (var i=0;i<newField.length;i++) {
+                            var theName = newField[i].name
+                            if (theName) {
+                                newField[i].name = theName + counter;
+                                newField[i].attributes.required = "required";
+                            }
+                        }
+                        var insertHere = document.getElementById('writeroot');
+                        insertHere.parentNode.insertBefore(newFields,insertHere);
+                        
+                    }
+                    
+                    function getCounter() {
+                        return counter;
+                    }
+                    window.onload = moreFields;
+                </script>
+                
+
                 <?php 
                     echo "<p>Club Name</p>";
                     echo "<p>Location</p>";
                     echo "<p>Meeting times</p>";
-                    echo "<br>";
-                    echo "<br>";
                 ?>
             </div>
             <div class="account" id="accountmid">
@@ -28,15 +57,14 @@
                     echo "<p>|</p>";
                     echo "<p>|</p>";
                     echo "<p>|</p>";
-                    echo "<br>";
-                    echo "<br>";
                 ?>
             </div>
             <div class="account" id="clubformright">
                 <p><input name="clubname" type="text" required></p>
                 <p><input name="location" type="text" required></p>
-                <p>
-                    <select name="meetingday1" required>
+                
+                <p id="readroot" style="display: none">
+                    <select name="meetingday" >
                         <option value="">Day of the Week...</option>
                         <option value="Monday">Monday</option>
                         <option value="Tuesday">Tuesday</option>
@@ -45,44 +73,16 @@
                         <option value="Friday">Friday</option>
                         <option value="Saturday">Saturday</option>
                         <option value="Sunday">Sunday</option>
-                    <select>
+                    </select>
                     from
-                    <input name="starttime1" type="time" required>
+                    <input name="starttime" type="time" >
                     to
-                    <input name="endtime1" type="time" required> 
-                </p>      
-                                <p>
-                    <select name="meetingday2">
-                        <option value="">Day of the Week...</option>
-                        <option value="Monday">Monday</option>
-                        <option value="Tuesday">Tuesday</option>
-                        <option value="Wednesday">Wednesday</option>
-                        <option value="Thursday">Thursday</option>
-                        <option value="Friday">Friday</option>
-                        <option value="Saturday">Saturday</option>
-                        <option value="Sunday">Sunday</option>
-                    <select>
-                    from
-                    <input name="starttime2" type="time">
-                    to
-                    <input name="endtime2" type="time"> 
-                </p> 
-                <p>
-                    <select name="meetingday3">
-                        <option value="">Day of the Week...</option>
-                        <option value="Monday">Monday</option>
-                        <option value="Tuesday">Tuesday</option>
-                        <option value="Wednesday">Wednesday</option>
-                        <option value="Thursday">Thursday</option>
-                        <option value="Friday">Friday</option>
-                        <option value="Saturday">Saturday</option>
-                        <option value="Sunday">Sunday</option>
-                    <select>
-                    from
-                    <input name="starttime3" type="time"> 
-                    to
-                    <input name="endtime3" type="time"> 
-                </p>   
+                    <input name="endtime" type="time" >
+                </p>
+                
+                <span id="writeroot"></span>
+                <input type="button" onclick="moreFields()" value="Give me more fields!" />
+ 
             </div>
             <div class="accountbutton" id="accountedit"><input class="accountbutton" id="accountsave" type="submit" value="Submit"></div>
         </form>
