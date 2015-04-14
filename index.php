51c3ecb0bd5d621a0f4ac69060d1ef4921fe0328
AaronC <chenga2@rpi.edu>
2015-04-14 05:42:11 -0400

    Homepage Renovation
    Changed homepage appearance and design. Will update the header file
    soon.

--- index.php
+++ index.php
@@ -1,79 +1,98 @@
                                 <?php
     session_start();
 ?>
-<?php 
-  include ( "header.php" ); 
-?>
-<html>
-<!--
-    <head>
-        <title>rClubs</title>
-        <link type="text/css" rel="stylesheet" href="css/main.css">
-  	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
-	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
- 	<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
-  	<script type="text/javascript" src="js/autocomplete.js"></script>
-    </head>
--->
-    <body>
-<!--
-        <div class="heading">
-            <a href="http://rclubs.me"><img class="homebutton" src="images/rClubs3.png"></a>
-            <ul>
-                <a href="http://rclubs.me/about"><li class="topbutton" id="left">About</li></a>
-                <a href="http://rclubs.me/feedback"><li class="topbutton" id="left">Feedback</li></a>
-                <a href="http://rclubs.me/allClubs"><li class="topbutton" id="left">All Clubs</li></a>
-                <a href="http://rclubs.me/signup"><li class="topbutton" id="right" style="margin-right:3vw;">
-                	<?php
-                	if (!isset($_SESSION['myusername']))
-    				{echo("Signup");}
-    			?>
-                </li></a>
-                <a href="http://rclubs.me/login"><li class="topbutton" id="right">
-                	<?php
-                	if (!isset($_SESSION['myusername']))
-    				{echo("Login");}
-    			?>
-                </li></a>
-                <a href="http://rclubs.me/login/logout.php"><li class="topbutton" id="right">
-                	<?php
-                	if (isset($_SESSION['myusername']))
-    				{echo("Logout");}
-    			?>
-                </li></a>
-                <a href="http://rclubs.me/myclubs/"><li class="topbutton" id="right">
-                	<?php
-                	if (isset($_SESSION['myusername']))
-    				{echo("MyClubs");}
-    			?>
-                </li></a>
-            </ul>
-        </div>
-        <div class="heading_underline"></div>
--->
-        <div class="search">
-            <div class="logo">
-                <center>
-                    <img src="/images/rClubs3.png" width="20%;">
-                </center>
-            </div>
-            <center>
-                <div class="ui-widget">
-                    <form method="post" action="php/display_result.php">
-                        <input name="mysearch" id="homesearchbar" type="text" placeholder="Search Clubs"/>
-                    </form>
-                </div>
-            </center>
-        </div>
-        <div class="footer">
-            <center>
-                <ul class="footbar">
-                    <a href="#"><li class="bottombutton"><font color="#af0000">r</font><font color="#797979">Clubs</font></li></a>
-                    <a href="http://rcos.rpi.edu/projects/rpi-tradebook/"><li class="bottombutton"><font color="#af0000">@RCOS</font></li></a>
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html">
+    <title>rClubs</title>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arvo">
+    <link rel="shortcut icon" href="http://static.tmimgcdn.com/img/favicon.ico">
+    <link rel="icon" href="http://static.tmimgcdn.com/img/favicon.ico">
+    <link rel="stylesheet" type="text/css" media="all" href="css/homepage.css">
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+</head>
+
+<body>
+    <nav>
+    <div class="wrapper">
+        <div class="logo"><a href="http://rclubs.me"><img  id="logoo" src="images/rClubs4.png"></a></div>
+            
+        <ul id="menu" class="clearfix">
+            <li><a href="http://rclubs.me">Home</a></li>
+            <li><a href="http://rclubs.me/about">About</a></li>
+            <li><a href="http://rclubs.me/allClubs">Browse Clubs</a></li>
+            
+            <?php if(!isset($_SESSION['myusername'])){ ?>
+            <li><a href="http://rclubs.me/login">Login</a></li>
+            <?php } ?> 
+            
+            <?php if(!isset($_SESSION['myusername'])){ ?>
+            <li><a href="http://rclubs.me/signup">Signup</a></li>
+            <?php } ?> 
+            
+            <?php if(isset($_SESSION['myusername'])){ ?>
+            <li><a href="#">Account</a>
+                <ul>
+                <li><a href="http://rclubs.me/account">Profile</a></li>
+                <li><a href="http://rclubs.me/myclubs">MyClubs</a></li>
+                <li><a href="http://rclubs.me/chat">Chat</a></li>
+                <li><a href="http://rclubs.me/login/logout.php">Logout</a></li>
+                <li><a href="#"></a></li>
                 </ul>
-            </center>
-        </div>
-    </body>
+            </li>
+            <?php } ?> 
+        </ul>
+    </div>
+    </nav>
+<script type="text/javascript">
+$(function(){
+  $('a[href="#"]').on('click', function(e){
+    e.preventDefault();
+  });
+  
+  $('#menu > li').on('mouseover', function(e){
+    $(this).find("ul:first").show();
+    $(this).find('> a').addClass('active');
+  }).on('mouseout', function(e){
+    $(this).find("ul:first").hide();
+    $(this).find('> a').removeClass('active');
+  });
+  
+  $('#menu li li').on('mouseover',function(e){
+    if($(this).has('ul').length) {
+      $(this).parent().addClass('expanded');
+    }
+    $('ul:first',this).parent().find('> a').addClass('active');
+    $('ul:first',this).show();
+  }).on('mouseout',function(e){
+    $(this).parent().removeClass('expanded');
+    $('ul:first',this).parent().find('> a').removeClass('active');
+    $('ul:first', this).hide();
+  });
+});
+</script>
+    
+    <div class="banner-container">
+        <header class="banner">
+            <h1>Introducing <span id="first-letter">r</span>Clubs! 
+                <span id="banner-text">Join <span id="first-letter">or</span> manage your clubs with ease.</span>
+                <span id="banner-text">Simple <span id="first-letter">&</span> Easy.</span>
+            </h1>
+            <a href="http://rclubs.me/signup" class="signup-button">SIGNUP</a>
+        </header>
+    </div>
+    
+<div class="home-search">
+<form method="post" action="../php/display_result.php" id="search">
+  <input name="mysearch" type="text" size="40" placeholder="search for clubs" />
+        </form>
+</div>
+    
+</body>
 </html>
                             
                             
